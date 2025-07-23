630844781ce678213c8292c21fb2c1f9c7042853
jwelyl <cork2586@naver.com>
2025-07-23 22:24:11 +0900

    [refactor] 헤더 구조 개편 및 스타일 통일
    - 로고와 버튼을 flex layout으로 정렬해 상단 전체 폭을 차지하도록 수정
    - 기존 중앙 정렬 방식 제거
    - "문제", "채점현황", "랭킹", "로그인/로그아웃", "회원가입/회원수정" 버튼을 우측 정렬로 통일
    - 버튼 스타일을 기존 헤더 스타일에 맞게 일관화
    - navigate("/") 클릭 로직을 로고 클릭으로 변경

--- src/common/LogoHeader.jsx
+++ src/common/LogoHeader.jsx
@@ -3,22 +3,14 @@ import { useNavigate, Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { logout } from "../store/slices/authSlice";
 
-const baseButtonClass =
-  "mx-2 my-2 px-5 py-2 text-sm rounded bg-blue-500 text-white hover:bg-blue-600 transition";
-
 const LogoHeader = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const { isLoggedIn } = useSelector((state) => state.auth);
 
-  const handleClick = () => {
-    navigate("/");
-  };
-
   const handleLogout = () => {
     dispatch(logout());
     window.location.reload();
-    // navigate("/");
   };
 
   const handleUpdate = () => {
@@ -26,31 +18,64 @@ const LogoHeader = () => {
   };
 
   return (
-    <div className="text-center bg-white border-b border-gray-200 select-none">
-      <div onClick={handleClick} className="py-6 text-3xl font-bold cursor-pointer">
-        🦆 알고오리
-      </div>
+    <div className="bg-white border-b border-gray-200">
+      <div className="flex items-center justify-between px-6 py-4">
+        {/* 왼쪽 로고 */}
+        <div
+          className="flex items-center space-x-3 cursor-pointer select-none"
+          onClick={() => navigate("/")}
+        >
+          <span className="text-3xl">🦆</span>
+          <span className="text-2xl font-bold">알고오리</span>
+        </div>
 
-      <div className="flex flex-wrap justify-center pb-4">
-        {isLoggedIn ? (
-          <>
-            <button onClick={handleLogout} className={baseButtonClass}>
-              로그아웃
+        {/* 오른쪽 모든 버튼 */}
+        <div className="flex flex-wrap items-center justify-end space-x-2">
+          <Link to="/problems">
+            <button className="px-4 py-2 text-sm text-white bg-blue-500 rounded hover:bg-blue-600">
+              문제
             </button>
-            <button onClick={handleUpdate} className={baseButtonClass}>
-              회원 수정
+          </Link>
+          <Link to="/submissions">
+            <button className="px-4 py-2 text-sm text-white bg-blue-500 rounded hover:bg-blue-600">
+              채점현황
             </button>
-          </>
-        ) : (
-          <>
-            <Link to="/login">
-              <button className={baseButtonClass}>로그인</button>
-            </Link>
-            <Link to="/signup">
-              <button className={baseButtonClass}>회원가입</button>
-            </Link>
-          </>
-        )}
+          </Link>
+          <Link to="/ranking">
+            <button className="px-4 py-2 text-sm text-white bg-blue-500 rounded hover:bg-blue-600">
+              랭킹
+            </button>
+          </Link>
+          {isLoggedIn ? (
+            <>
+              <button
+                onClick={handleLogout}
+                className="px-4 py-2 text-sm text-white bg-blue-500 rounded hover:bg-blue-600"
+              >
+                로그아웃
+              </button>
+              <button
+                onClick={handleUpdate}
+                className="px-4 py-2 text-sm text-white bg-blue-500 rounded hover:bg-blue-600"
+              >
+                회원 수정
+              </button>
+            </>
+          ) : (
+            <>
+              <Link to="/login">
+                <button className="px-4 py-2 text-sm text-white bg-blue-500 rounded hover:bg-blue-600">
+                  로그인
+                </button>
+              </Link>
+              <Link to="/signup">
+                <button className="px-4 py-2 text-sm text-white bg-blue-500 rounded hover:bg-blue-600">
+                  회원가입
+                </button>
+              </Link>
+            </>
+          )}
+        </div>
       </div>
     </div>
   );
